8cb649423a6858c14dc13c622a6534d10d076372
RahulRST <srahuljayaraj@gmail.com>
2023-10-31 19:53:22 +0530

    dynamic json and error control and modularization

--- Code/PTO_Arduino/PTO_Arduino.ino
+++ Code/PTO_Arduino/PTO_Arduino.ino
@@ -1,59 +1,109 @@
 #include <ArduinoJson.h>
-#include <TinyGPSPlus.h>
+#include <TinyGPS++.h>
 #include <ArduinoMqttClient.h>
 #include <ESP8266WiFi.h>
 
 #define IRPIN_IN 9
 #define IRPIN_OUT 10
 
-StaticJsonDocument<100> doc;
+DynamicJsonDocument doc(200);
 TinyGPSPlus gps;
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
-const char broker[] = "192.168.198.179";
+const char broker[] = "192.168.29.170";
 int port = 1883;
 const char topic[] = "pto_data";
 int totalPas = 0;
+unsigned long previousMillis = 0;
+const long interval = 10000;  // 10 seconds interval
+
+void addLocationData(JsonDocument &doc, float latitude, float longitude) {
+  JsonObject location = doc.createNestedObject("location");
+  location["latitude"] = latitude;
+  location["longitude"] = longitude;
+}
+
+void addRidershipData(JsonDocument &doc, int totalPassengers) {
+  JsonObject ridership = doc.createNestedObject("ridership");
+  ridership["total"] = totalPassengers;
+}
+
+void connectWiFi() {
+  Serial.print("Connecting to WiFi");
+  WiFi.begin("Rahul's Galaxy S20+", "Rahul@123");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi!");
+}
+
+bool connectToMQTT() {
+  int attempt = 0;
+  while (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    if (attempt > 5) {
+      Serial.println("Exceeded maximum attempts. Please check MQTT broker.");
+      return false;
+    }
+    delay(2000);
+    attempt++;
+  }
+  Serial.println("Connected to MQTT broker!");
+  return true;
+}
 
 void setup() {
   Serial.begin(115200);
   pinMode(IRPIN_IN, INPUT);
   pinMode(IRPIN_OUT, INPUT);
-  Serial.print("Connecting to WiFi");
-  WiFi.begin("Rahul's Galaxy S20+","Rahul@123");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  connectWiFi();
+  
+  if (!connectToMQTT()) {
+    while (1) {
+      // Stay in a loop if MQTT connection fails for safety measures
+      delay(1000);
+    }
   }
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-
-    while (1)
-      ;
-  }
-  Serial.println("You're connected to the MQTT broker!");
-  Serial.println();
 }
+
 void loop() {
   mqttClient.poll();
-  mqttClient.beginMessage(topic);
-  float lat = gps.location.lat();
-  float lon = gps.location.lng();
-  doc.clear();
-  JsonObject loc = doc.createNestedObject("location");
-  loc["latitude"] = lat;
-  loc["longitude"] = lon;
-  if (digitalRead(IRPIN_IN) == HIGH) {
-    totalPas++;
+  unsigned long currentMillis = millis();
+
+  while (Serial.available() > 0) {
+    if (gps.encode(Serial.read())) {
+      break;
+    }
   }
-  if (digitalRead(IRPIN_OUT) == HIGH) {
-    totalPas--;
+
+  if (!mqttClient.connected()) {
+    if (!connectToMQTT()) {
+      // Handle reconnection attempts or safety measures here
+      // For now, we will just try to reconnect
+      return;
+    }
   }
-  JsonObject rider = doc.createNestedObject("ridership");
-  rider["total"] = totalPas;
-  serializeJson(doc, mqttClient);
-  mqttClient.endMessage();
-  delay(10000);
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    mqttClient.beginMessage(topic);
+
+    addLocationData(doc, gps.location.lat(), gps.location.lng());
+
+    if (digitalRead(IRPIN_IN) == HIGH) {
+      totalPas++;
+    }
+    if (digitalRead(IRPIN_OUT) == HIGH) {
+      totalPas--;
+    }
+
+    addRidershipData(doc, totalPas);
+
+    serializeJson(doc, mqttClient);
+    mqttClient.endMessage();
+    doc.clear();
+  }
 }
